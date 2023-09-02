193552e31386e75500d27e95135a0b6deaeb84d3
Barbaro Javier <barbaro.valmaseda@gmail.com>
2023-09-02 10:58:49 +0400

    Deploy API to FL0

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const crypto = require("node:crypto");
+const cors = require("cors");
+const movies = require("./movies.json");
+const { validateMovie, validatePartialMovie } = require("./schema/movies");
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:1234",
+        "http://localhost:8080",
+        "http://localhost:8081",
+        "http://localhost:3000",
+        "https://movies.com",
+      ];
+      if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+app.disable("x-powered-by");
+
+// all resource movies are identified with /movies
+app.get("/movies", (req, res) => {
+  const { genre } = req.query;
+
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
+
+  res.json(movies);
+});
+
+app.get("/movies/:id", (req, res) => {
+  //path-to-regex
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+  if (movie) return res.json(movie);
+  res.status(404).json({ message: "Movies not found" });
+});
+
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    // 422 Unprocessed entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  app.delete("/movies/:id", (req, res) => {
+    const { id } = req.params;
+    const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+    if (movieIndex === -1) res.status(404).json({ message: "Movie not found" });
+    movies.splice(movieIndex, 1);
+
+    return res.json({ message: "Movie deleted" });
+  });
+
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid V4
+    ...result.data,
+  };
+
+  movies.push(newMovie);
+  res.status(201).json(newMovie);
+});
+
+app.patch("/movies/:id", (req, res) => {
+  //path-to-regex
+  const result = validatePartialMovie(req.body);
+
+  if (result.error)
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+  if (movieIndex === -1) res.status(404).json({ message: "Movie not found" });
+
+  const updateMovie = { ...movies[movieIndex], ...result.data };
+  movies[movieIndex] = updateMovie;
+  return res.json(updateMovie);
+});
+
+const PORT = process.env.PORT ?? 1234;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
